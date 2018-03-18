1620d505bb47e42f0bd0b8cc3d38d048fd398a05
samuel <berriens@gmail.com>
2018-03-18 22:17:34 +0100

    dotCuda3 marche -> + performant qu'à grande echelle pour l'instant...

--- src/cuda/cuvec.cu
+++ src/cuda/cuvec.cu
@@ -27,7 +27,28 @@ __global__ void reduce0(float* g_odata, float* g_idata1, float* g_idata2) {
 	}
 }
 
-__global__ void dotCuda(float* tmp, float* t1, float* t2, int size, int fake_size) {
+// https://stackoverflow.com/questions/26853363/dot-product-for-dummies-with-cuda-c
+__global__ void dotCuda3(float *a, float *b, float *c){
+	__shared__ float cache[1024];
+	//int tid = threadIdx.x + blockIdx.x*blockDim.x; 
+	int tid = blockIdx.x * (blockDim.x*2) + threadIdx.x;
+	int cacheIndex = threadIdx.x; 
+	float temp = a[tid] * b[tid] + a[tid + blockDim.x] * b[tid + blockDim.x];
+	cache[cacheIndex] = temp; 
+	__syncthreads(); 
+
+	for (unsigned int i = blockDim.x >> 1; i > 0; i >>= 1) {
+    	if(cacheIndex < i)
+        	cache[cacheIndex] += cache[cacheIndex + i];
+        __syncthreads();      
+	}
+
+	if (cacheIndex == 0){ 
+    	c[blockIdx.x] = cache[0]; 
+	}
+}
+
+__global__ void dotCuda(float* tmp, float* t1, float* t2, int size) {
 	//unsigned int tid = threadIdx.x;
 	unsigned int i = blockIdx.x * blockDim.x + threadIdx.x;
 
@@ -35,8 +56,6 @@ __global__ void dotCuda(float* tmp, float* t1, float* t2, int size, int fake_siz
 	__syncthreads();
 
 	int mididx = size / 2;
-
-	//bool needCorrect = mididx % 2 == 1;
 
 	while (i < mididx) {
 		tmp[i] += tmp[i + mididx];
@@ -98,33 +117,40 @@ float CudaVec::dot(CudaVec other) {
 	}
 	//tmp[0] = 0;
 
-	int fake_size = (int) pow(2, ceil(log(this->size)/log(2)));
+	//int fake_size = (int) pow(2, ceil(log(this->size)/log(2)));
 
 	int nbBlX = getNbBlockDimX(0);
 	int thrPBl = getNbThreadPerBlock(0);
 
-	int neededBl = fake_size / nbBlX;
+	int neededBl = this->size / thrPBl / 2;
 
 	float* tmp;
 	cudaMalloc(&tmp, sizeof(float) * neededBl);
+	cudaMemset(tmp, 0, sizeof(float) * neededBl);
 
-	if (fake_size <= thrPBl) {
-		dotCuda<<<1, fake_size>>>(tmp, this->cudaptr, other.cudaptr, this->size);
+	if (this->size <= thrPBl) {
+		//dotCuda<<<1, fake_size>>>(tmp, this->cudaptr, other.cudaptr, this->size);
+		//Dev_dot<<<1, this->size>>>(this->cudaptr, other.cudaptr, tmp, this->size);
 		//reduce0<<<1, this->size>>>(tmp, this->cudaptr, other.cudaptr);
+		cout << "pas sur que ça marche ! 1 block of " << this->size << " threads" << endl;
+		dotCuda3<<<1, this->size>>>(this->cudaptr, other.cudaptr, tmp);
 	} else {
 		
-		//cout << "pb appel, started : " << (neededBl * thrPBl) << ", curr : " << this->size << endl;
+		cout << "pb appel, started : " << (neededBl * thrPBl) << ", curr : " << this->size << endl;
 
 		if (neededBl <= nbBlX) {
-			dotCuda<<<neededBl, thrPBl>>>(tmp, this->cudaptr, other.cudaptr, this->size);
+			//Dev_dot<<<neededBl, thrPBl>>>(this->cudaptr, other.cudaptr, tmp, this->size);
+			//dotCuda<<<neededBl, thrPBl>>>(tmp, this->cudaptr, other.cudaptr, this->size);
 			//reduce0<<<neededBl, thrPBl>>>(tmp, this->cudaptr, other.cudaptr);
+			dotCuda3<<<neededBl, thrPBl>>>(this->cudaptr, other.cudaptr, tmp);
 		} else {
 			cout << "Unimplemented nbBlNeeded : " << neededBl << " (" << nbBlX << " available)" << endl;
 			exit(0);
 		}
 	}
+	cudaThreadSynchronize();
 
-	/*float* result = (float*) malloc(sizeof(float) * neededBl);
+	float* result = new float[neededBl];
 	// Le resultat est dans tmp[0]
 	cudaMemcpy(result, tmp, sizeof(float) * neededBl, cudaMemcpyDeviceToHost);
 	cudaFree(tmp);
@@ -134,9 +160,9 @@ float CudaVec::dot(CudaVec other) {
 		sum += result[i];
 	}
 
-	return sum;*/
-	float result = 0.f;
+	return sum;
+	/*float result = 0.f;
 	cudaMemcpy(&result, tmp, sizeof(float) * 1, cudaMemcpyDeviceToHost);
 	cudaFree(tmp);
-	return result;
+	return result;*/
 }
