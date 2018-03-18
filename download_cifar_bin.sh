8f3e8a0a91cf7f0ae2767adf3f0a16012812bb63
samuel <berriens@gmail.com>
2018-03-18 22:33:57 +0100

    fix script

--- download_cifar_bin.sh
+++ download_cifar_bin.sh
@@ -3,7 +3,7 @@ if ! [ -d "./res" ]; then
 	mkdir res
 fi
 cd res
-if ! [ -d "./cifar-10-batches-bin"]; then
+if ! [ -d "cifar-10-batches-bin/" ]; then
 	wget https://www.cs.toronto.edu/~kriz/cifar-10-binary.tar.gz
 	tar xvzf cifar-10-binary.tar.gz
 	rm -f cifar-10-binary.tar.gz
