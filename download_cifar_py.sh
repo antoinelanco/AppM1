defad0cc5733053463cb04f6956892d7e27c98a8
samuel <berriens@gmail.com>
2018-03-18 22:16:16 +0100

    add script to downnload cifar python

---
+++ download_cifar_py.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+if ! [ -d "./res" ]; then
+	mkdir res
+fi
+cd res
+if ! [ -d "./cifar-10-batches-py"]; then
+	wget https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz
+	tar xvzf cifar-10-binary.tar.gz
+	rm -f cifar-10-binary.tar.gz
+fi
+cd ..
