0e22f611117692d5128e59b7c4250b5ce1a753d0
samuel <berriens@gmail.com>
2018-03-20 23:02:25 +0100

    commencé class perceptron

--- src/model/perceptron.cpp
+++ src/model/perceptron.cpp
@@ -1,1 +1,2 @@
 #include "perceptron.h"
+
--- src/model/perceptron.h
+++ src/model/perceptron.h
@@ -1,4 +1,17 @@
 #ifndef PERCEPTRON_H
 #define PERCEPTRON_H
 
+#include "../cuda/cuvec.h"
+
+class Perceptron {
+private:
+	CudaVec weights;
+	int nbClass;
+	int inputSize;
+	float learning_rate;
+public:
+	Perceptron(int inputSize, int nbClass, float lr);
+	int predict();
+};
+
 #endif
