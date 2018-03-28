dc647b9b8437d58a1a0c70062f44218420db3ecb
antoinelanco <antoinelanco@gmail.fr>
2018-03-28 11:05:13 +0200

    kmeans

---
+++ src/model/k_means.h
@@ -0,0 +1,19 @@
+#ifndef K_MEANS_H
+#define K_MEANS_H
+
+#include <vector>
+#include "../utils/data.h"
+
+class K_means {
+private:
+  /* data */
+
+public:
+  K_means (vector<data> v;);
+  virtual ~K_means ();
+};
+
+
+
+
+#endif
