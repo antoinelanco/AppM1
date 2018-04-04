f4a898e8952d55fb6b3c657cf6a02a92c4d8ce99
samuel <berriens@gmail.com>
2018-04-04 11:44:57 +0200

    séparé fonction print img

--- src/main.cpp
+++ src/main.cpp
@@ -7,23 +7,24 @@
 #include "model/k_means.h"
 #include "data/gather_data.h"
 
+void printImg(data toprint) {
+	for (int i = 0; i < 32; i++) {
+		for (int j = 0; j < 32; j++) {
+			float moy = 0.f;
+			moy = max(moy, toprint.features[(i * 32 + j) * 3]);
+			moy = max(moy, toprint.features[(i * 32 + j) * 3 + 1]);
+			moy = max(moy, toprint.features[(i * 32 + j) * 3 + 2]);
+			cout << ( moy < 0.5f ? ". " : "# ");
+		}
+		cout << endl;
+	}
+	cout << "label : " << toprint.label << endl;
+}
+
 void approcheNaive() {
 	vector<img_brute> d = read_batch(getResFolder() + "/cifar-10-batches-bin/data_batch_1.bin", 10000);
 
 	vector<data> batch_data = transform_to_data(d);
-	// int toprint = batch_data.size() - 1;
-	// cout << batch_data.size() << endl;
-	// for (int i = 0; i < 32; i++) {
-	// 	for (int j = 0; j < 32; j++) {
-	// 		float moy = 0.f;
-	// 		moy = max(moy, batch_data[toprint].features[(i * 32 + j) * 3]);
-	// 		moy = max(moy, batch_data[toprint].features[(i * 32 + j) * 3 + 1]);
-	// 		moy = max(moy, batch_data[toprint].features[(i * 32 + j) * 3 + 2]);
-	// 		cout << ( moy < 0.5f ? ". " : "# ");
-	// 	}
-	// 	cout << endl;
-	// }
-	// cout << "label : " << batch_data[toprint].label << endl;
 
 	vector<img_brute> img_test = read_batch(getResFolder() + "/cifar-10-batches-bin/test_batch.bin", 1000);
 	vector<data> data_test = transform_to_data(img_test);
