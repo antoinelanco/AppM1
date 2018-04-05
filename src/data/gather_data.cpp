25bc2e0256901bf38d2acf5bec4fb397bed8278d
Ipsedo <7268@samu>
2018-04-05 21:17:33 +0200

    adapt gather data to n patches data

--- src/data/gather_data.cpp
+++ src/data/gather_data.cpp
@@ -1,46 +1,34 @@
 #include "gather_data.h"
 #include <iostream>
 
-vector<data> gatherDataFeatures(K_means k_means_patch, vector<data> splittedData, int N) {
+vector<data> gatherDataFeatures(K_means k_means_patch, vector<data> splittedData, int N, int nbPatch) {
   vector<data> gatherDataFeatures;
-  for (int i = 0; i < splittedData.size(); i += 4) {
-    data fst = splittedData[i];
-    data snd = splittedData[i + 1];
-    data thr = splittedData[i + 2];
-    data fth = splittedData[i + 3];
-    int fst_pred = k_means_patch.predict(fst);
-    int snd_pred = k_means_patch.predict(snd);
-    int thr_pred = k_means_patch.predict(thr);
-    int fth_pred = k_means_patch.predict(fth);
+  for (int i = 0; i < splittedData.size(); i += nbPatch) {
+    int label = splittedData[i].label;
     data res;
-    res.features = vector<float>();
-    for (int j = 0; j < N; j++) {
-        res.features.push_back(fst_pred == j ? 1.f : 0.f);
+    res.label = label;
+    for (int j = 0; j < nbPatch; j++) {
+      data curr = splittedData[i + j];
+      if (curr.label != label) {
+        cout << "Not same label of " << nbPatch << " consecutive pathes !" << endl;
+        exit(1);
+      }
+
+      int pred = k_means_patch.predict(curr);
+      for (int k = 0; k < N; k++) {
+          res.features.push_back(pred == k ? 1.f : 0.f);
+      }
     }
-    for (int j = 0; j < N; j++) {
-        res.features.push_back(snd_pred == j ? 1.f : 0.f);
-    }
-    for (int j = 0; j < N; j++) {
-        res.features.push_back(thr_pred == j ? 1.f : 0.f);
-    }
-    for (int j = 0; j < N; j++) {
-        res.features.push_back(fth_pred == j ? 1.f : 0.f);
-    }
-    if (fst.label != snd.label && snd.label != thr.label && thr.label != fth.label) {
-      cout << "Not same label of 4 consecutive pathes !" << endl;
-      exit(1);
-    }
-    res.label = fst.label;
     gatherDataFeatures.push_back(res);
   }
   return gatherDataFeatures;
 }
 
-pair<vector<data>, K_means > trainKMeansDataFeatures(vector<data> splittedData, int N, int nbIter) {
+pair<vector<data>, K_means > trainKMeansDataFeatures(vector<data> splittedData, int N, int nbIter, int nbPatch) {
   K_means k_means(N, splittedData);
   k_means.proc(nbIter);
 
-  vector<data> newData = gatherDataFeatures(k_means, splittedData, N);
+  vector<data> newData = gatherDataFeatures(k_means, splittedData, N, nbPatch);
 
   return make_pair(newData, k_means);
 }
--- src/data/gather_data.h
+++ src/data/gather_data.h
@@ -9,13 +9,13 @@
 using namespace std;
 
 /**
- *  Retourne des data de 4 * N fetures déduite de k_means_patch
+ *  Retourne des data de nbPatch * N fetures déduite de k_means_patch
  */
-vector<data> gatherDataFeatures(K_means k_means_patch, vector<data> splittedData, int N);
+vector<data> gatherDataFeatures(K_means k_means_patch, vector<data> splittedData, int N, int nbPatch);
 
 /**
- *  Retourne des data de 4 * N features et le K_means appris sur ces données
+ *  Retourne des data de nbPatch * N features et le K_means appris sur ces données
  */
-pair<vector<data>, K_means > trainKMeansDataFeatures(vector<data> splittedData, int N, int nbIter);
+pair<vector<data>, K_means > trainKMeansDataFeatures(vector<data> splittedData, int N, int nbIter, int nbPatch);
 
 #endif
