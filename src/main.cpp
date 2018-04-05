d25a98b32ff0d454e542cc8026c64dce71da5ded
Ipsedo <7268@samu>
2018-04-05 21:18:22 +0200

    test mnist, k-means 91% d'erreur :,(

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 
 #include "utils/res.h"
 #include "data/read_cifar.h"
+#include "data/read_mnist.h"
 #include "data/data.h"
 #include "model/perceptron.h"
 #include "model/k_means.h"
@@ -45,37 +46,79 @@ void approcheNaive() {
 }
 
 void approcheDesBoss() {
+	int nbPatch = 4;
+
 	cout << "Loading Data..." <<endl;
 	vector<img_brute> d = read_batch(getResFolder() + "/cifar-10-batches-bin/data_batch_1.bin", 1000);
 	vector<data> batch_data = transform_to_data(d);
 
 	cout << "Split data..." << endl;
-	vector<data> splittedData = split(batch_data);
-	int N = 20;
+	vector<data> splittedData = split(batch_data, nbPatch);
+	int N = 2048;
 	cout << "Learn K-Means..." << endl;
-	pair<vector<data>, K_means > resGather = trainKMeansDataFeatures(splittedData, N, 10);
+	pair<vector<data>, K_means > resGather = trainKMeansDataFeatures(splittedData, N, 100, nbPatch);
 
 	cout << "Learn Perceptron..." << endl;
-	Perceptron p(N * 4, 10, 0.1);
-	int nbEpoch = 1000;
+	Perceptron p(N * nbPatch, 10, 0.1);
+	int nbEpoch = 100;
 	for (int i = 0; i < nbEpoch; i++) {
 		cout << '\r' << ((i+1.)/nbEpoch)*100 << "%" << std::flush;
 		p.update(resGather.first);
 	}
-
 
 	cout << "\nTest..." << endl;
 
 	vector<img_brute> img_test = read_batch(getResFolder() + "/cifar-10-batches-bin/test_batch.bin", 1000);
 	vector<data> data_test = transform_to_data(img_test);
 
-	vector<data> splittedTestImg = split(data_test);
-	vector<data> featuresData = gatherDataFeatures(resGather.second, splittedTestImg, N);
+	vector<data> splittedTestImg = split(data_test, nbPatch);
+	vector<data> featuresData = gatherDataFeatures(resGather.second, splittedTestImg, N, nbPatch);
 
 	cout << "Score : " << p.score(featuresData) << endl;
 }
 
+void test() {
+	cout << "Loading Data..." <<endl;
+	vector<img_brute> d = read_batch(getResFolder() + "/cifar-10-batches-bin/data_batch_1.bin", 1);
+	vector<data> batch_data = transform_to_data(d);
+
+	int nbPatch = 16; // Must be power of 2;
+	vector<data> splittedData = split(batch_data, nbPatch);
+	cout << splittedData.size() << endl;
+	for (int i = 0; i < splittedData.size(); i++) {
+		cout << splittedData[i].features.size() << endl;
+	}
+}
+
+void mnist() {
+	cout << "Loading data..." << endl;
+	vector<data> trainData = read_mnist(getResFolder() + "/mnist/train-images-idx3-ubyte",
+	 getResFolder() + "/mnist/train-labels-idx1-ubyte",
+	 1000);
+
+	vector<data> testData = read_mnist(getResFolder() + "/mnist/t10k-images-idx3-ubyte",
+	 getResFolder() + "/mnist/t10k-labels-idx1-ubyte",
+	 1000);
+	cout << "Perceptron training..." << endl;
+	Perceptron p(28 * 28, 10, 0.1);
+ 	int nbEpoch = 100;
+ 	for (int i = 0; i < nbEpoch; i++) {
+ 		cout << '\r' << ((i+1.)/nbEpoch)*100 << "%" << flush;
+ 		p.update(trainData);
+ 	}
+	cout << endl;
+
+	cout << "Score : " << p.score(testData) << endl << endl;
+
+	cout << "K-Means training..." << endl;
+	K_means k(10, trainData);
+	k.proc(10);
+	cout << "Score : " << k.loss(testData) << endl;
+}
+
 int main(int argc, char** argv) {
-	approcheDesBoss();
+	//approcheDesBoss();
+	//test();
+	mnist();
 	return 0;
 }
