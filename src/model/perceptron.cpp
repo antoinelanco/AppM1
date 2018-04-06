e396da4a375dc85c741155d2ef485fd89453e1fc
Ipsedo <7268@samu>
2018-04-06 22:20:09 +0200

    print repartition prediction

--- src/model/perceptron.cpp
+++ src/model/perceptron.cpp
@@ -2,6 +2,8 @@
 #include <limits>
 #include <math.h>
 #include <iostream>
+#include <tuple>
+#include <algorithm>
 
 #include <vector>
 
@@ -21,7 +23,7 @@ Perceptron::Perceptron(int inputSize, int nbClass, float lr) {
 
 int Perceptron::predict(data d) {
 	int argmax = -1;
-	float max = numeric_limits<float>::min();
+	float max = -numeric_limits<float>::max();
 	for (int i = 0; i < this->weights.size(); i++) {
 		float sum = 0.f;
 		for (int j = 0; j < this->weights[i].size(); j++) {
@@ -35,13 +37,24 @@ int Perceptron::predict(data d) {
 	return argmax;
 }
 
+bool pairCompare(pair<int, int>& firstElem, pair<int, int>& secondElem) {
+  return firstElem.first < secondElem.first;
+}
+
 float Perceptron::score(vector<data> d) {
 	float err = 0.f;
 	float total = 0.f;
+	vector<pair<int, int>> res;
 	for (data curr_d : d) {
-		if (this->predict(curr_d) != curr_d.label)
+		pair<int, int> a(this->predict(curr_d), curr_d.label);
+		if (a.first != a.second)
 			err++;
 		total++;
+		res.push_back(a);
+	}
+	sort(res.begin(), res.end(), pairCompare);
+	for (int i = 0; i < res.size(); i++) {
+		cout << res[i].first << " " << res[i].second << endl;
 	}
 	return err / total;
 }
@@ -60,8 +73,8 @@ float* Perceptron::th_vec(vector<float> input) {
 
 void Perceptron::update(vector<data> d) {
 	for (data curr_d : d) {
-		if (this->predict(curr_d) == curr_d.label)
-			continue;
+		// if (this->predict(curr_d) == curr_d.label)
+		// 	continue;
 		float* g = this->th_vec(curr_d.features);
 		for (int k = 0; k < this->weights.size(); k++) {
 			float etiquette = curr_d.label == k ? 1.f : -1.f;
