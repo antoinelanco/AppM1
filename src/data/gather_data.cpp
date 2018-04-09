74ef0c2d140d82b7643fa7a9397705bdbd7eb2a4
Ipsedo <7268@samu>
2018-04-09 21:53:07 +0200

    suppr print -> plus besoin

--- src/data/gather_data.cpp
+++ src/data/gather_data.cpp
@@ -17,15 +17,10 @@ vector<data> gatherDataFeatures(K_Means_2 k_means_patch, vector<data> splittedDa
       }
 
       int pred = k_means_patch.predict(curr);
-      if (pred >= 0) count[pred]++;
-      else cout << "bug" << endl;
+      count[pred]++;
       res.features[j * N + pred] = 1.f;
     }
     dataFeatures.push_back(res);
-  }
-  cout << "Gather" << endl;
-  for (int i = 0; i < count.size(); i++) {
-    cout << i << " " << count[i] << endl;
   }
   return dataFeatures;
 }
